e58cadb77a09f7fc08e0eb49ff75f9b084d18b7e
Jefferson Quesado <jefferson.neto@trustly.com>
2026-05-21 22:17:19 -0300

    mais testes por estado

--- frete.test.js
+++ frete.test.js
@@ -4,3 +4,18 @@ it('deve calcular frete de 20 para PE', async () => {
     const vrFrete = await frete.__calculoDoCep('123', (__) => Promise.resolve({ data: { uf: 'PE'}}))
     expect(vrFrete).toBe(20)
 })
+
+it('deve calcular frete de 40 para CE', async () => {
+    const vrFrete = await frete.__calculoDoCep('123', (__) => Promise.resolve({ data: { uf: 'CE'}}))
+    expect(vrFrete).toBe(40)
+})
+
+it('deve calcular frete de 5 para SP', async () => {
+    const vrFrete = await frete.__calculoDoCep('123', (__) => Promise.resolve({ data: { uf: 'SP'}}))
+    expect(vrFrete).toBe(5)
+})
+
+it("deve calcular 'CEPINV' caso a API retorne um erro", async () => {
+    const vrFrete = await frete.__calculoDoCep('123', (__) => Promise.resolve({ data: { erro: 'erro'}}))
+    expect(vrFrete).toBe('CEPINV')
+})
